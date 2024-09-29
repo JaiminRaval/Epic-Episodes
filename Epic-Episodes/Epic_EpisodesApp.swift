1b193e49aa5ec524b819c8cc3c25800a8e2e1c9b
JaiminRaval <siliconvalley.jaimin@gmail.com>
2024-09-29 21:53:35 +0530

    Initial Commit

---
+++ Epic-Episodes/Epic_EpisodesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Epic_EpisodesApp.swift
+//  Epic-Episodes
+//
+//  Created by Jaimin Raval on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Epic_EpisodesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
